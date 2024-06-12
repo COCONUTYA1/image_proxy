6b1c2ae5948bd87cfc8cfbf883f4daae7689871a
COCONUTYA1 <36963837+COCONUTYA1@users.noreply.github.com>
2024-06-12 15:59:26 +0800

    Create pull.sh

---
+++ pull.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+read -p "请输入用户名:" USERNAME
+read -p "请输入密码:" PASSWORD
+echo $PASSWORD  | docker login --username=$PASSWORD --password-stdin registry.cn-hangzhou.aliyuncs.com
+for i in $(cat list.txt); 
+do 
+  docker pull registry.cn-hangzhou.aliyuncs.com/cachei/${i##*/}  ;
+  docker tag registry.cn-hangzhou.aliyuncs.com/cachei/${i##*/} $i  ; 
+  docker rmi registry.cn-hangzhou.aliyuncs.com/cachei/${i##*/}
+done
+docker logout registry.cn-hangzhou.aliyuncs.com
